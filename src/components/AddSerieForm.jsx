c1177c2c6b05bc0c4d0e7727ffb9973b8635c3b5
Miguelco23 <miguelco23vzv@gmail.com>
2023-02-04 17:52:14 -0500

    Components separated

---
+++ src/components/AddSerieForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function AddSerieForm() {
+    const [newSerie, setNewSerie] = useState({ name: "", img: "" });
+
+    const handleInputs = (event) => {
+        const { name, value } = event.target;
+
+        setNewSerie((prev) => {
+            return {...prev, [name]: value };
+        });
+    };
+
+    const addSerie = (event) => {
+        alert(newSerie.name + " has been added to the database!"); 
+    }
+    return (
+        <div className="AddSerieForm form">
+            <label htmlFor="addSerieForm"><h3>Add a new serie</h3></label>
+            <div className="form-floating mb-3">
+                <input type="text" name="name" className="form-control" id="floatingInput" placeholder="Game on thrones" onChange={handleInputs} />
+                <label htmlFor="floatingInput">Serie's name</label>
+            </div>
+            <div className="form-floating">
+                <input type="text" name="img" className="form-control" id="floatingPassword" placeholder="image link" onChange={handleInputs} />
+                <label htmlFor="floatingPassword">Image's link</label>
+            </div>
+            <button id="addSerie" className="btn btn-outline-success" onClick={addSerie}>Add new serie</button>
+        </div>
+    );
+}
+
+export default AddSerieForm;
--- src/components/MyMovies.jsx
+++ src/components/MyMovies.jsx
@@ -1,55 +1,15 @@
 import { useState } from "react";
-function MyMovies() {
-    let mySeries = [
-        { "id": 1, "name": "Naruto", "img": "https://i.imgur.com/LJd6LQu.jpeg" },
-        { "id": 2, "name": "Naruto Shippuden", "img": "https://i.imgur.com/bmiCtQ9.png" },
-        { "id": 3, "name": "Soul eater", "img": "https://upload.wikimedia.org/wikipedia/en/f/fe/Soul_Eater_manga_volume_1.jpg" },
-        { "id": 4, "name": "No game no life", "img": "https://m.media-amazon.com/images/M/MV5BZTI1MjY3N2YtODczMy00MGQwLWI2NWMtODQwZTE3NTY5OTExXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg" },
-        { "id": 5, "name": "Elite", "img": "https://m.media-amazon.com/images/M/MV5BYmU5N2YzYjQtM2Q4Yi00NWY4LWI5NDQtMzE4OTlhY2Q5NmJlXkEyXkFqcGdeQXVyMTMxNjUyMDkx._V1_.jpg" },
-        { "id": 6, "name": "Sword art online", "img": "https://es.web.img3.acsta.net/pictures/19/07/09/11/04/5921608.jpg" },
-        { "id": 7, "name": "Big mouth", "img": "https://images.justwatch.com/poster/257258766/s718/big-mouth.%7Bformat%7D" },
-        { "id": 8, "name": "El robo del siglo", "img": "http://www.elespectadorimaginario.com/wp-content/webpc-passthru.php?src=http://www.elespectadorimaginario.com/wp-content/uploads/el_robo_del_siglo_1.jpg&nocache=1" },
-        { "id": 9, "name": "Solar opposites", "img": "https://lumiere-a.akamaihd.net/v1/images/solar_opposites_4x5_las_e0333d41.png" },
-        { "id": 10, "name": "Alice in borderland", "img": "" },
-        { "id": 11, "name": "Super campeones", "img": "" },
-        { "id": 12, "name": "Blue exorcist", "img": "" },
-        { "id": 13, "name": "Maniac", "img": "" },
-        { "id": 14, "name": "Castlevania", "img": "" },
-        { "id": 15, "name": "Devilman crybaby", "img": "" },
-        { "id": 16, "name": "B the beginning ", "img": "" },
-        { "id": 17, "name": "One punch man", "img": "" },
-        { "id": 18, "name": "Rick y morty", "img": "" },
-        { "id": 19, "name": "Little witch academia", "img": "" },
-        { "id": 20, "name": "Tokyo Ghoul", "img": "" },
-        { "id": 21, "name": "7 pecados capitales", "img": "" },
-        { "id": 22, "name": "Los Simpsons", "img": "" },
-        { "id": 23, "name": "Bojackn Horseman", "img": "" },
-        { "id": 24, "name": "Final space", "img": "" },
-        { "id": 25, "name": "Love death & robots", "img": "" },
-        { "id": 26, "name": "Hellsing", "img": "" },
-        { "id": 27, "name": "Mirai Nikk", "img": "" },
-        { "id": 28, "name": "Los juegos del calamar", "img": "" }
-    ];
-
-    const [series, setSeries] = useState(mySeries);
-
-    const addSerie = () => {
-        alert("A new serie has been added to the database");
-    }
-
-
-
-
-
+function MyMovies({list}) {
+   
     return (
         <div className="MyMovies">
             <header>
                 <h1>ApdFlix</h1>
-                <h2>Miguel has seen {series.length} series</h2>
-                <a className="btn btn-success d-flex me-2" href="#addSerieForm">Add a new serie</a>
+                <h2>Miguel has seen {list.length} series</h2>
+                <a className="btn btn-success" href="#addSerieForm" id="header-button">Add a new serie</a>
             </header>
             <div className="series-grid">
-                {series.map((serie, index) => {
+                {list.map((serie, index) => {
                     return (
                         <div className="card" key={index}>
                             <img src={serie.img} className="card-img-top" alt={serie.name} />
@@ -60,20 +20,7 @@ function MyMovies() {
                     );
                 })
                 }
-            </div>
-            <div className="lines"></div>
-            <div id="addSerieForm" className="form">
-                <label for="addSerieForm"><h3>Add a new serie</h3></label>
-                <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="floatingInput" placeholder="Game on thrones" />
-                    <label for="floatingInput">Serie's name</label>
-                </div>
-                <div className="form-floating">
-                    <input type="text" className="form-control" id="floatingPassword" placeholder="image link" />
-                    <label for="floatingPassword">Image's link</label>
-                </div>
-                <button id="addSerie" className="btn btn-outline-success" onClick={addSerie}>Add new serie</button>
-            </div>
+            </div>  
         </div>
     );
 }
